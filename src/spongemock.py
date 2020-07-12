eab2ec102a5a94cdb62cd1152b344a582a657ee2
edwcmiller3 <edwcmiller3@outlook.com>
2020-07-12 00:25:33 -0400

    First try

--- src/spongemock.py
+++ src/spongemock.py
@@ -1,2 +1,9 @@
-def mock_text(text):
-    return False
+def mock_text(user_text):
+    user_text_list = list(user_text.lower())
+    for i in range(0, len(user_text_list), 2):
+        if not user_text_list[i].isalpha():
+            i -= 1
+        user_text_list[i] = user_text_list[i].upper()
+    return ''.join(user_text_list)
+
+mock_text('this is a test')
